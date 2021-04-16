2e1d42e2b04a721654e11bc63e892f090803c892
tviljane <79187708+tviljane@users.noreply.github.com>
2021-04-16 11:46:04 +0300

    Add files via upload

---
+++ Viikko2ja3/Viikko2/ehto2lauseita1/teht1/v2.js
@@ -0,0 +1,32 @@
+
+function tarkistus()
+{
+var numero = document.getElementById('numero').value;
+
+var tulos = numero
+
+
+if (numero <0)
+{
+  tulos = "negatiivinen";
+} else if (numero >=0) {
+  tulos = "positiivinen";
+} else {
+  tulos = "Anna luku"
+}
+
+document.getElementById('vastaus').innerHTML = tulos;
+}
+
+
+viikonpäivät
+
+
+function paiva()
+{
+  var vnumero
+
+
+
+
+}
